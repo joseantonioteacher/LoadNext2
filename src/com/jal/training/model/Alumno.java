b4a83cbb987bf374aeb04c0ce7735fb786b179d9
joseantonioteacher <joseantoniolp.teacher@gmail.com>
2022-04-08 12:33:25 +0200

    Añadido toString

--- src/com/jal/training/model/Alumno.java
+++ src/com/jal/training/model/Alumno.java
@@ -1,9 +1,16 @@
 package com.jal.training.model;
 
+
 public class Alumno {
+	
 	private Long id = null;
 	
 	public Alumno() {
 		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+"{id="+id+"}";
 	}
 }
